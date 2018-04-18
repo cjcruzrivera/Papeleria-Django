4df6d7a4b7daeebc9d64cac9223bbf00adbc3f79
invitado <invitado@s3pc17.eisc.univalle.edu.co>
2018-04-18 15:37:26 -0500

    creacion de los modelos y sus migraciones

--- practica_orm/settings.py
+++ practica_orm/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_orm'
 ]
 
 MIDDLEWARE = [
@@ -75,9 +76,18 @@ WSGI_APPLICATION = 'practica_orm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '1535038',
+        'USER': '1535038',
+        'PASSWORD': '1535038',
+        'HOST': 'pgsql2',
+        'PORT': '5432',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
