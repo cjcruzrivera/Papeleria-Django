43a30aba5b2c5a9b4248c030ccba085e13ed761e
cjcruzrivera <cruz.camilo@correounivalle.edu.co>
2018-04-26 23:57:31 -0500

    update de cliente y inicio delete usando ajax

--- app_clientes/urls.py
+++ app_clientes/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from .views import index, ClienteCreateView, ClienteListView
+from .views import index, ClienteCreateView, ClienteListView, ClienteUpdateView
 
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^nuevo/$', ClienteCreateView.as_view(), name='nuevo'),
     url(r'^listar/$', ClienteListView.as_view(), name='listar'),
+    url(r'^editar/(?P<pk>\d+)/$', ClienteUpdateView.as_view(), name='editar'),
 ]
--- app_clientes/views.py
+++ app_clientes/views.py
@@ -22,3 +22,9 @@ class ClienteCreateView(CreateView):
 class ClienteListView(ListView):
     model = Cliente
     template_name = "cliente/cliente_list.html"
+
+class ClienteUpdateView(UpdateView):
+    model = Cliente
+    form_class = ClienteForm
+    template_name = "cliente/cliente_form.html"
+    success_url = reverse_lazy('clientes:listar')
