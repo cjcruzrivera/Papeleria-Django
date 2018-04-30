2b913c9eddbc585f874c95e7287d61e58260027c
cjcruzrivera <cruz.camilo@correounivalle.edu.co>
2018-04-29 22:50:15 -0500

    Inicio Frontend Ventas

--- app_ventas/views.py
+++ app_ventas/views.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from app_clientes.models import Cliente
+from app_productos.models import Producto
 # Create your views here.
+
+def index(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'ventas/index.html',{
+        'clientes':clientes
+    })
